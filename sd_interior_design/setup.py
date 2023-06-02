8009d18008e16313eedf5ac0295c72de6db4e57e
Ryan Dick <ryanjdick3@gmail.com>
2023-06-02 17:51:42 -0400

    Copy train_text_to_image_lora.py from https://github.com/huggingface/diffusers/blob/2faf91dbdeb51ad41e8a398d16818932374cde0c/examples/text_to_image/train_text_to_image_lora.py

---
+++ sd_interior_design/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="text_to_image_lora",
+    version="0.1",
+    description="A library for fine-tuning text-t-image Stable Diffusion model with LoRA.",
+    author="Ryan Dick",
+    author_email="ryanjdick3@gmail.com",
+    packages=["text_to_image_lora"],
+)
