9c93370af3c57fdae1e04423bb1e9207096a9b61
Ryan Dick <ryanjdick3@gmail.com>
2024-02-27 21:25:22 -0500

    Add migrationFile struct to be used by a DB migration tool.

---
+++ go-htmx-tailwind/internal/db/migration_file.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"fmt"
+	"path/filepath"
+	"regexp"
+	"strconv"
+)
+
+type migrationFile struct {
+	filePath string
+	name     string
+	version  int
+}
+
+func (m *migrationFile) FilePath() string { return m.filePath }
+func (m *migrationFile) Name() string     { return m.name }
+func (m *migrationFile) Version() int     { return m.version }
+
+var integerRegex = regexp.MustCompile(`^\d+`)
+
+func newMigrationFile(filePath string) (*migrationFile, error) {
+	_, fileName := filepath.Split(filePath)
+	ext := filepath.Ext(fileName)
+	if ext != ".sql" {
+		return nil, fmt.Errorf("file %s is not a .sql file", filePath)
+	}
+
+	name := fileName[:len(fileName)-len(ext)]
+
+	versionString := integerRegex.FindString(fileName)
+	version, err := strconv.Atoi(versionString)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse version from migration file: %w", err)
+	}
+
+	return &migrationFile{
+		filePath: filePath,
+		name:     name,
+		version:  version,
+	}, nil
+}
